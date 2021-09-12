afb9e3122b09abd0a6b4d956d790914f6ecf8b5e
EmmanueleTorres <emmanueletorres@gmail.com>
2021-09-12 19:06:29 -0300

    character model-crud

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,21 +5,43 @@ class CharactersController < ApplicationController
   end
 
   def show
+    @character = Character.find(params[:id])
   end
 
   def new
+    @character = Character.new
   end
   
   def create
+    @character = Character.new(character_params)
+    @character.save
+    redirect_to character_path(@character)
   end
 
   def edit
+    @character = Character.find(params[:id])
   end
   
   def update
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to character_path(@character)
   end
   
   def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to characters_path
+  end
+  
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :age, :weight, :history)
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
   end
   
 end
