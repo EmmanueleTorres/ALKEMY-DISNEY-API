aac0d8765fbd77dfbec0d552e690b4f54152aa2b
EmmanueleTorres <emmanueletorres@gmail.com>
2021-09-29 11:57:22 -0300

    added conection character to film through casts

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -1,17 +1,33 @@
 class CastsController < ApplicationController
   def new
-    @film = Film.find(params[:film_id])
-    @cast = Cast.new
+    if params[:film_id].present?
+      @film = Film.find(params[:film_id])
+      @cast = Cast.new
+    else
+      @character = Character.find(params[:character_id])
+      @cast = Cast.new
+    end
   end
 
   def create
-    @film = Film.find(params[:film_id])
-    @cast = Cast.new(cast_params)
-    @cast.film = @film
-    if @cast.save
-      redirect_to film_path(@film)
+    if params[:film_id].present?
+      @film = Film.find(params[:film_id])
+      @cast = Cast.new(cast_film_params)
+      @cast.film = @film
+      if @cast.save
+        redirect_to film_path(@film)
+      else
+        render :new
+      end
     else
-      render :new
+      @character = Character.find(params[:character_id])
+      @cast = Cast.new(cast_character_params)
+      @cast.character = @character
+      if @cast.save
+        redirect_to character_path(@character)
+      else
+        render :new
+      end
     end
   end
 
@@ -23,7 +39,11 @@ class CastsController < ApplicationController
   
   private
 
-  def cast_params
+  def cast_film_params
     params.require(:cast).permit(:comment, :character_id)
+  end
+
+  def cast_character_params
+    params.require(:cast).permit(:comment, :film_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :characters
+  resources :characters do
+    resources :casts, only: [:new, :create]
+  end
+
   resources :films do
     resources :casts, only: [:new, :create]
   end
+  
   resources :casts, only: :destroy
   resources :genres, only: :index
 end
